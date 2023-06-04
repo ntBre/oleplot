610c7ac32dd76cb268c32d5bdd3968ed77beb0f6
Brent Westbrook <brentrwestbrook@gmail.com>
2023-06-03 21:10:35 -0400

    add lib.rs and break off modules

---
+++ src/colors.rs
@@ -0,0 +1,7 @@
+use super::Color;
+use image::Rgb;
+
+pub const WHITE: Color = Rgb([255, 255, 255]);
+pub const BLACK: Color = Rgb([0, 0, 0]);
+pub const GREEN: Color = Rgb([0, 255, 0]);
+pub const GREY: Color = Rgb([222, 222, 222]);
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+use image::Rgb;
+
+pub type Color = Rgb<u8>;
+pub type Point = (u32, u32);
+
+pub mod colors;
+pub mod shapes;

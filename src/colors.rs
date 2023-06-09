5998d2f8031ed6c17390518e96047cf26a607d0e
Brent Westbrook <brentrwestbrook@gmail.com>
2023-06-09 15:22:29 -0400

    add red, blue, and cyan colors

--- src/colors.rs
+++ src/colors.rs
@@ -3,5 +3,8 @@ use image::Rgb;
 
 pub const WHITE: Color = Rgb([255, 255, 255]);
 pub const BLACK: Color = Rgb([0, 0, 0]);
+pub const RED: Color = Rgb([255, 0, 0]);
 pub const GREEN: Color = Rgb([0, 255, 0]);
+pub const BLUE: Color = Rgb([0, 0, 255]);
+pub const CYAN: Color = Rgb([0, 255, 255]);
 pub const GREY: Color = Rgb([222, 222, 222]);
